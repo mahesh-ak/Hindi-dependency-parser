db53e364e58eb3efdae576945cd289cd3c88d20c
maheshak <mahesh.akavarapu@gmail.com>
2020-02-01 15:27:58 +0530

    added webwork

---
+++ app.py
@@ -0,0 +1,70 @@
+try:
+    from flask import Flask
+
+    from flask import Flask, request, render_template, redirect, url_for, session, send_file
+
+    from flask_wtf import FlaskForm, RecaptchaField
+    from wtforms import StringField, SubmitField, RadioField, DateTimeField, SelectField, TextAreaField
+
+    from wtforms.validators import DataRequired
+
+    from flask import session
+    from spacy import displacy
+    from flaskext.markdown import Markdown
+
+except Exception as e:
+    print("Some Modules are Missing")
+
+app = Flask(__name__)
+Markdown(app)
+app.config["SECRET_KEY"] = 'mysecretkey'
+
+
+class Widgets(FlaskForm):
+
+    Statement = StringField(label="STATEMENT")
+
+    submit = SubmitField(label="Submit")
+
+
+def foo(value):
+    print("Work to be done")
+
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    form = Widgets()
+    if request.method == 'POST':
+        if (form.validate_on_submit()):
+            val = form.Statement.data
+            print(val)
+            session['data'] = val
+            # return render_template('home.html', form=form)
+            return redirect('/thanks')
+    return render_template('home.html', form=form)
+
+
+@app.route("/thanks", methods=["GET", "POST"])
+def thanks():
+    val = session['data']
+    # newval = foo(val)
+    ex = [{
+        "words": [
+            {"text": "This", "tag": "DT"},
+            {"text": "is", "tag": "VBZ"},
+            {"text": "a", "tag": "DT"},
+            {"text": "sentence", "tag": "NN"}
+        ],
+        "arcs": [
+            {"start": 0, "end": 1, "label": "nsubj", "dir": "left"},
+            {"start": 2, "end": 3, "label": "det", "dir": "left"},
+            {"start": 1, "end": 3, "label": "attr", "dir": "right"}
+        ]
+    }]
+    htm = displacy.render(ex, manual="True")
+    print(htm)
+    return render_template('thanks.html', val=val, html=htm)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
