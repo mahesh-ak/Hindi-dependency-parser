52772e5c18290e5c7d4cc492f4a76699283f1794
maheshak <mahesh.akavarapu@gmail.com>
2020-02-05 12:25:54 +0530

    cache problem resolved

--- app.py
+++ app.py
@@ -33,6 +33,18 @@ def foo(value):
     print("Work to be done")
 
 
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
+    
 @app.route("/", methods=["GET", "POST"])
 def home():
 
